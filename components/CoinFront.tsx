9005509d0bebe061265d390c56d8b68c0d76ff7f
dezwashere <dezd8008@gmail.com>
2026-09-24 15:22:01 -0700

    Remove legacy cross shape data

--- components/CoinFront.tsx
+++ components/CoinFront.tsx
@@ -13,7 +13,6 @@ const PATHS: Record<string,string> = {
   shield:"M50 2 L100 17 L100 65 L50 100 L0 65 L0 17 Z",
   diamond:"M50 2 L95 50 L50 100 L5 50 Z",
   star:"M50 2 L61 35 L98 35 L68 57 L79 91 L50 70 L21 91 L32 57 L2 35 L39 35 Z",
-  cross:"M33 0 L67 0 L67 33 L100 33 L100 67 L67 67 L67 100 L33 100 L33 67 L0 67 L0 33 L33 33 Z",
   badge:"M50 0 L65 10 L82 5 L90 20 L100 30 L95 50 L100 70 L90 80 L82 95 L65 90 L50 100 L35 90 L18 95 L10 80 L0 70 L5 50 L0 30 L10 20 L18 5 L35 10 Z",
   arrow:"M0 35 L55 35 L55 10 L100 50 L55 90 L55 65 L0 65 Z",
 };
@@ -25,7 +24,6 @@ const BOUNDS: Record<string,{minX:number;maxX:number;minY:number;maxY:number;wid
   shield:{minX:.03,maxX:.97,minY:.02,maxY:.98,width:.94,height:.96},
   diamond:{minX:.04,maxX:.96,minY:.03,maxY:.97,width:.92,height:.94},
   star:{minX:.02,maxX:.98,minY:.02,maxY:.98,width:.96,height:.96},
-  cross:{minX:0,maxX:1,minY:0,maxY:1,width:1,height:1},
   badge:{minX:.02,maxX:.98,minY:.02,maxY:.98,width:.96,height:.96},
   arrow:{minX:0,maxX:1,minY:.35,maxY:.65,width:1,height:.30},
 };
