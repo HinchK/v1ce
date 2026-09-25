a26ed7c6a13c7954c9ef74e6ec582290faebd4ac
dezwashere <dezd8008@gmail.com>
2026-09-24 19:23:52 -0700

    Trim launch font loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,9 @@
 import {
   Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold,
 } from "@expo-google-fonts/inter";
-import { BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
-import { BodoniModa_700Bold } from "@expo-google-fonts/bodoni-moda";
-import { Cinzel_700Bold } from "@expo-google-fonts/cinzel";
-import { CourierPrime_700Bold } from "@expo-google-fonts/courier-prime";
-import { DMSans_700Bold } from "@expo-google-fonts/dm-sans";
-import { Fredoka_400Regular } from "@expo-google-fonts/fredoka";
-import { IBMPlexSerif_700Bold } from "@expo-google-fonts/ibm-plex-serif";
-import { Pacifico_400Regular } from "@expo-google-fonts/pacifico";
-import { Poppins_700Bold } from "@expo-google-fonts/poppins";
-import { SpaceMono_700Bold } from "@expo-google-fonts/space-mono";
-import { Syne_700Bold } from "@expo-google-fonts/syne";
 import { RobotoMono_700Bold } from "@expo-google-fonts/roboto-mono";
-import { Arimo_700Bold } from "@expo-google-fonts/arimo";
 import { Oswald_600SemiBold } from "@expo-google-fonts/oswald";
 import { Raleway_700Bold } from "@expo-google-fonts/raleway";
-import { Saira_700Bold } from "@expo-google-fonts/saira";
-import { JosefinSans_600SemiBold } from "@expo-google-fonts/josefin-sans";
 import { Fraunces_700Bold } from "@expo-google-fonts/fraunces";
 import { Caveat_400Regular } from "@expo-google-fonts/caveat";
 import { DynaPuff_600SemiBold } from "@expo-google-fonts/dynapuff";
@@ -45,23 +31,14 @@ function RootNavigation() {
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold,
-    BebasNeue_400Regular, BodoniModa_700Bold, Cinzel_700Bold, CourierPrime_700Bold,
-    DMSans_700Bold, Fredoka_400Regular, IBMPlexSerif_700Bold, Pacifico_400Regular,
-    Poppins_700Bold, SpaceMono_700Bold, Syne_700Bold,
-    RobotoMono_700Bold, Arimo_700Bold, Oswald_600SemiBold, Raleway_700Bold, Saira_700Bold,
-    JosefinSans_600SemiBold, Fraunces_700Bold, Caveat_400Regular, DynaPuff_600SemiBold,
+    RobotoMono_700Bold, Oswald_600SemiBold, Raleway_700Bold,
+    Fraunces_700Bold, Caveat_400Regular, DynaPuff_600SemiBold,
     BigShouldersStencilDisplayRegular: require("../assets/fonts/BigShouldersStencilDisplay-Regular.ttf"),
-    SedgwickAveDisplayRegular: require("../assets/fonts/SedgwickAveDisplay-Regular.ttf"),
     RobotoMonoWidget: require("../assets/fonts/native/RobotoMono-Variable.ttf"),
-    ArimoWidget: require("../assets/fonts/native/Arimo-Variable.ttf"),
     OswaldWidget: require("../assets/fonts/native/Oswald-Variable.ttf"),
     RalewayWidget: require("../assets/fonts/native/Raleway-Variable.ttf"),
-    SairaWidget: require("../assets/fonts/native/Saira-Variable.ttf"),
-    EduQLDHandWidget: require("../assets/fonts/native/EduQLDHand-Variable.ttf"),
-    JosefinSansWidget: require("../assets/fonts/native/JosefinSans-Variable.ttf"),
     FrauncesWidget: require("../assets/fonts/native/Fraunces-Variable.ttf"),
     CaveatWidget: require("../assets/fonts/native/Caveat-Regular.ttf"),
-    GeistPixelWidget: require("../assets/fonts/native/GeistPixel-Variable.ttf"),
     DynaPuffWidget: require("../assets/fonts/native/DynaPuff-Variable.ttf"),
   });
 
